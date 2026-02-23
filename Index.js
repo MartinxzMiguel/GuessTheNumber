f17a75b7f903062afbc319baddf10628f7a113e8
MartinxzMiguel <miguel.m2k4@gmail.com>
2026-02-22 23:15:18 -0500

    The project is created, only exercise information

---
+++ Index.js
@@ -0,0 +1,16 @@
+// Guess the Number
+/* Object - Player 
+    Attributes
+        -> Number to guess
+        -> Attemps
+        -> Status
+*/
+/* Functions 
+    Traditional
+        -> Evaluate Number
+        -> Repeat Attemps
+
+    Arrow
+        -> Generate Random Number
+        -> Show Information
+*/

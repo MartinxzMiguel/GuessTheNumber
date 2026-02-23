ac5f460cc966cec26122d519eb5bb0268e9d8ecf
MartinxzMiguel <miguel.m2k4@gmail.com>
2026-02-22 23:22:54 -0500

    The traditional function is created to evaluate the number

--- Index.js
+++ Index.js
@@ -28,3 +28,25 @@ const GenerateRandomNumber = () => Math.floor(Math.random() * 10) + 1; // Number
 
 // Arrow Function --> Show Information
 const ShowInfo = (Message) => console.log(Message);
+
+// Traditional Function --> Evaluate Number
+function EvaluateNumber(UserGuess) {
+    Player.Attempts++; // Attemps Auto-Increment 
+
+    // Conditional if they are the same
+    if (UserGuess == Player.NumberToGuess) {
+        Player.Status = "Won";
+        ShowInfo(`          Attempt ${Player.Attempts}: ${UserGuess} = ${Player.NumberToGuess} ---> True`);
+        ShowInfo(" -----------------------------------------------");
+        ShowInfo(`|     YOU GUESSED THE NUMBER ${UserGuess} IN ${Player.Attempts} ATTEMPS     |`);
+        ShowInfo(" -----------------------------------------------");
+        return true;
+    } 
+
+    // Conditional if they are different
+    if (UserGuess != Player.NumberToGuess) {
+        Player.Status = "You lose";
+        ShowInfo(`          Attempt ${Player.Attempts}: ${UserGuess} = ${Player.NumberToGuess} ---> False`);
+    }
+    return false;
+}
